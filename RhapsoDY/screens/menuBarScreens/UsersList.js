fb666ad91f9ea20fa4b36924378d9d278939a076
Leng Jin De, Joel <joellengjd@gmail.com>
2022-07-07 15:41:00 +0800

    .

--- RhapsoDY/screens/menuBarScreens/UsersList.js
+++ RhapsoDY/screens/menuBarScreens/UsersList.js
@@ -1,21 +1,32 @@
 import { db } from "../../firebase";
 import { auth } from "../../firebase";
 import { ref, onValue, get } from "firebase/database";
+import { onAuthStateChanged } from "firebase/auth";
 
 export default function UsersList() {
     const dbUserRef = ref(db, 'users/');
     const users = [];
     onValue(dbUserRef, (snapshot) => {
-        const uuid = auth.currentUser.uid;
         snapshot.forEach((child) => {
-            if (child.val().uuid == uuid) {
-
-            } else {
-                users.push({
-                    userName: child.val().email, //using email as name is not done up yet
-                    uuid: child.val().uuid
-                })
-            }
+            onAuthStateChanged(auth, (user) => {
+                if (user) {
+                  // User is signed in, see docs for a list of available properties
+                  // https://firebase.google.com/docs/reference/js/firebase.User
+                  const uid = user.uid;
+                  if (child.val().uuid == uid) {
+                    //
+                  } else {
+                    users.push({
+                        userName: child.val().email, //using email as name is not done up yet
+                        uuid: child.val().uuid
+                    })
+                }
+                
+                } else {
+                    // User is signed out
+                    // ...
+                  }
+                });
         });
         console.log("List of Users", users);
 
